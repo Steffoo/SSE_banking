84bce385b32a94a23a6561eec06992ab040c68b8
SonnyBoy3428 <sonnyboy3428@gmail.com>
2018-01-03 00:00:51 +0100

    Started implementing the backend, added gitignore

---
+++ Backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const winston = require('winston');
+
+const app = express();
+
+var frontendDir;
+var logFile = './data/log/server.log';
+
+winston.configure({
+	transports: [
+		new (winston.transports.File)({filename: logFile}),
+		new (winston.transports.Console)({timestamp: true})
+	]
+});
+
+if(process.env['RUNNING_VIA_DOCKER']) {
+	winston.log('Info', 'Running inside a docker environment');
+	frontendDir = './public';
+} else {
+	winston.log('Info', 'Running outside a docker environment');
+	frontendDir = '../frontend';
+}
+
+app.use(express.static(path.join(__dirname, frontendDir)));
+
+app.set('port', 3000);
+
+app.listen(app.get('port'), function(){
+	winston.log('Info', 'Listening on port ' + app.get('port'));
+})
