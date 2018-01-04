a0fa9cfb02b2d33372a2f756bb49494d7add4ef1
Dennis <d.czogalla@gmx.de>
2018-01-04 20:35:00 +0100

    update inserts

---
+++ Backend/data/sql/inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO account VALUES ('DE89370400440532013000','Igor','Wild','WilderIgi','Musterstrasse 42 12345 Musterhausen','06123 54214','igor@mail.de','admin',1500,0,NULL);
+INSERT INTO account VALUES ('DE89370400440532013500','Thomas','Müller','Thommy','Musterstrasse 2 25932 Musterhausen','06122 12214','igor@mail.de','admin',750.24,0,NULL);
+INSERT INTO account VALUES ('DE10370400440532013500','Horst','Günther','Günthi','Musterstrasse 39 54321 Musterhausen','06111 44432','igor@mail.de','admin',-239.50,1,"overdraft");
