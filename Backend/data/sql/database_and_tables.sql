9ff9042204d7902f493b20cb85ebbdf29b701414
Dennis <d.czogalla@gmx.de>
2018-01-04 19:09:06 +0100

    sql create commands

--- Backend/data/sql/database_and_tables.sql
+++ Backend/data/sql/database_and_tables.sql
@@ -1,3 +1,24 @@
 CREATE DATABASE IF NOT EXISTS sse_banking;
 
-CREATE TABLE IF NOT EXISTS konto (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, password VARCHAR(20) NOT NULL, iban VARCHAR(22) NOT NULL);
+CREATE TABLE IF NOT EXISTS acount ( iban VARCHAR(50) NOT NULL UNIQUE,
+                                    firstName VARCHAR(50) NOT NULL,
+                                    name VARCHAR(50) NOT NULL,
+                                    password VARCHAR(20) NOT NULL DEFAULT MD5('asdfg'),
+                                    adress VARCHAR(100) NOT NULL,
+                                    telephoneNumber VARCHAR(50) NOT NULL,
+                                    email VARCHAR(50) NOT NULL,
+                                    balance DECIMAL(9,2) NOT NULL,
+                                    locked BIT DEFAULT 0,
+                                    reasonForLock VARCHAR(100),
+                                    PRIMARY KEY(iban)
+                                  );
+
+CREATE TABLE IF NOT EXISTS accountmovement (iban_sender VARCHAR(50) NOT NULL,
+                                            iban_recipient VARCHAR(50) NOT NULL,
+                                            amount DECIMAL(9,2) NOT NULL,
+                                            date Date,
+                                            PRIMARY KEY(iban_sender),
+                                            PRIMARY KEY(iban_recipient),
+                                            FOREIGN KEY(iban_sender) REFERENCES account(iban),
+                                            FOREIGN KEY(iban_recipient) REFERENCES account(iban)
+                                            )
