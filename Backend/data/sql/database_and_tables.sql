df4b1d194daa1066812f2386aa790a0884b2efc2
Dennis <d.czogalla@gmx.de>
2018-01-04 20:02:29 +0100

    update create table statements

--- Backend/data/sql/database_and_tables.sql
+++ Backend/data/sql/database_and_tables.sql
@@ -1,13 +1,14 @@
-CREATE DATABASE IF NOT EXISTS sse_banking;
+CREATE DATABASE IF NOT EXISTS sse_banking use sse_banking;
 
-CREATE TABLE IF NOT EXISTS account ( iban VARCHAR(50) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS account
+                                  ( iban VARCHAR(50) NOT NULL UNIQUE,
                                     firstName VARCHAR(50) NOT NULL,
                                     name VARCHAR(50) NOT NULL,
                                     username VARCHAR(50) NOT NULL,
                                     adress VARCHAR(100) NOT NULL,
                                     telephoneNumber VARCHAR(50) NOT NULL,
                                     email VARCHAR(50) NOT NULL,
-                                    password VARCHAR(20) NOT NULL DEFAULT MD5('asdfg'),
+                                    password VARCHAR(20) NOT NULL,
                                     balance DECIMAL(9,2) NOT NULL,
                                     locked BIT DEFAULT 0,
                                     reasonForLock VARCHAR(100),
@@ -19,8 +20,7 @@ CREATE TABLE IF NOT EXISTS accountmovement (iban_sender VARCHAR(50) NOT NULL,
                                             amount DECIMAL(9,2) NOT NULL,
                                             purpose VARCHAR(50),
                                             date Date,
-                                            PRIMARY KEY(iban_sender),
-                                            PRIMARY KEY(iban_recipient),
+                                            PRIMARY KEY(iban_sender, iban_recipient),
                                             FOREIGN KEY(iban_sender) REFERENCES account(iban),
                                             FOREIGN KEY(iban_recipient) REFERENCES account(iban)
                                             )
